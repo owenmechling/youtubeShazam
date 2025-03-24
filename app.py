c86c5aa58bcc2dc8a11715dc8540733d0e610a2d
Owen Mechling <owen.mechling@trojans.dsu.edu>
2025-03-24 02:01:44 -0500

    Fixed a port binding issue

--- app.py
+++ app.py
@@ -90,3 +90,13 @@ def search_phrase():
 if __name__ == "__main__":
     app.run(debug=True)
 
+#------
+# PORT NAVIGATION
+#-----------
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
+
